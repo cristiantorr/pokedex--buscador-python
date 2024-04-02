d2d28d68c3e97f72c446ec730a87500346c666b0
Cristian Torres <catorres@smdigital.com.co>
2024-04-01 20:35:43 -0500

    Se crea buscador pokedex con API python

---
+++ pokedex.py
@@ -0,0 +1,44 @@
+import flet as ft
+import requests
+import base64
+from urllib.request import urlopen
+from PIL import Image
+from io import BytesIO
+
+def main(page):
+  logo_pokedex = ft.Image(
+    src=f"logo.png",
+    width=350,
+    height=170
+  )
+  nombre = ft.TextField(label="Nombre", autofocus=True)
+  submit = ft.ElevatedButton("Consultar")
+
+  poke_image = ft.Image(
+    src=f"background.png",
+    width=350,
+    height=350
+  )
+
+  def btn_click(e):
+    api_url_pokemon = f'https://pokeapi.co/api/v2/pokemon/{nombre.value}'
+    result = requests.get(api_url_pokemon)
+    if result.status_code == 200:
+      poke_data = result.json()
+      url_image = poke_data['sprites']['other']['official-artwork']['front_default']
+      im = Image.open(urlopen(url_image))
+      buffer = BytesIO()
+      im.save(buffer, format="png")
+      image_base64 = base64.b64encode(buffer.getvalue()).decode()
+      poke_image.src_base64 = image_base64
+      poke_image.update()
+
+  submit.on_click = btn_click
+  page.add(
+      logo_pokedex,
+      nombre,
+      submit,
+      poke_image
+    )
+
+ft.app(target=main)
